a94926cc67f95329d59560de15e72b7d853dfb75
ELECOU50 <144897011+ELECOU50@users.noreply.github.com>
2024-10-17 14:33:05 +0200

    Update grille.sh

--- grille.sh
+++ grille.sh
@@ -1,97 +1,128 @@
 #!/bin/bash
 
-# Initialisation des variables
-grille_x=0
-grille_y=0
-ligne=0
-colonne=0
+# Dimensions par défaut
+ROWS=20
+COLS=20
 
-# On vérifie que les valeurs saisies pour créer la grille sont entre 1 et 9
-while [[ "$grille_x" != +([1-9]) ]] || [ "$grille_x" -gt 9 ]; do
-        # Demande à l'utilisateur de saisir une valeur
-        dialog --inputbox "Longueur du plateau de jeu :" 8 40 2>output.txt
+# Grille principale et nouvelle grille pour l'évolution
+declare -A grid
+declare -A newGrid
 
-        # Récupère la valeur saisie
-        grille_x=$(<output.txt)
-
-        if [[ "$grille_x" != +([1-9]) ]] || [ "$grille_x" -gt 9 ]; then
-                dialog --title "Error" --msgbox "La valeur $grille_x n'est pas valide" 6 40
-        else
-                # Affiche la valeur saisie dans une boîte de message
-                dialog --msgbox "Valeur saisie : $grille_x" 6 40
-        fi
-
-        # Nettoie le fichier temporaire
-        rm -f output.txt
-done
-
-while [[ "$grille_y" != +([1-9]) ]] || [ "$grille_y" -gt 9 ]; do
-        # Demande à l'utilisateur de saisir une valeur
-        dialog --inputbox "Hauteur du plateau de jeu :" 8 40 2>output.txt
-
-        # Récupère la valeur saisie
-        grille_y=$(<output.txt)
-
-        if [[ "$grille_y" != +([1-9]) ]] || [ "$grille_y" -gt 9 ]; then
-                dialog --title "Error" --msgbox "La valeur $grille_y n'est pas valide" 6 40
-        else
-                # Affiche la valeur saisie dans une boîte de message
-                dialog --msgbox "Valeur saisie : $grille_y" 6 40
-        fi
-
-        # Nettoie le fichier temporaire
-        rm -f output.txt
-done
-
-# Fonction pour afficher la grille avec dialog
-display_grid() {
-    local display_grid
-
-    grille="-"
-    # Boucle pour initialiser la limite haute de la grille
-    for((z=0;z<grille_x;z++));do
-        grille+="--"
-    done
-    # Boucle pour afficher les lignes de la grille
-    for((y=0;y<grille_y;y++));do
-        grille+="\n| |"
-        # Boucle pour afficher les colonnes de la grille
-        for((i=1;i<grille_x;i++));do
-                grille+=" |"
-        done
-        grille+="\n-"
-        # Boucle pour afficher la séparation entre chaque ligne
-        for((z=0;z<grille_x;z++));do
-                grille+="--"
+# Fonction pour initialiser la grille avec des valeurs aléatoires (0 ou 1)
+InitGrid(){
+    for ((i=0; i<$ROWS; i++)); do
+        for ((j=0; j<$COLS; j++)); do
+            grid[$i,$j]=0
         done
     done
 
-    # Affichage de la grille
-    dialog --clear --title "Grille de Jeu" --msgbox "$grille" 30 30
-    source ./jouer.sh
+    # Placer des planeurs dans la grille
+    grid[1,2]=1
+    grid[2,3]=1
+    grid[3,1]=1
+    grid[3,2]=1
+    grid[3,3]=1
+    grid[15,16]=1
+    grid[16,17]=1
+    grid[17,15]=1
+    grid[17,16]=1
+    grid[17,17]=1
 }
 
-sous_menu2() {
-        local sous_menu2
-        # Boucle infinie
-        while true; do
-                # Affichage du menu
-                choix=$(dialog --clear --title "Voulez vous quitter le jeu ?" \
-                        --menu " " 17 50 2 \
-                        1 "Oui" \
-                        2 "Non" \
-                        2>&1 >/dev/tty)
+# Fonction pour afficher la grille dans une boîte de dialogue
+DisplayGrid() {
+    local output=""
 
-                        case $choix in
-                        1) dialog --msgbox "Retour au menu principal" 5 40 ; clear
-                           menu_principal ;;
-                        2) dialog --msgbox "Retour au jeu" 5 40
-                           display_grid ;;
-                        *) dialog --msgbox "Option invalide" 5 40 ;;
-                esac
+    # Dessiner la ligne supérieure de la grille
+    output+="+"
+    for ((j=0; j<$COLS; j++)); do
+        output+="-"
+    done
+    output+="+\n"
+
+    # Dessiner les lignes de la grille avec les cellules
+    for ((i=0; i<$ROWS; i++)); do
+        output+="|"
+        for ((j=0; j<$COLS; j++)); do
+            if [[ ${grid[$i,$j]} -eq 1 ]]; then
+                output+="█"  # Cellule vivante
+            else
+                output+=" "  # Cellule morte (espace vide)
+            fi
         done
+        output+="|\n"
+    done
+
+    # Dessiner la ligne inférieure de la grille
+    output+="+"
+    for ((j=0; j<$COLS; j++)); do
+        output+="-"
+    done
+    output+="+\n"
+
+
+    $finalRows=$ROWS+6
+    $finalCols=$COLS+6
+    dialog --msgbox "$output" "$finalRows" "$finalCols"
 }
 
-# Exécute les fonctions ci dessous
-display_grid
-sous_menu2
+# Fonction pour compter les voisins vivants d'une cellule
+CountNeighbors() {
+    local x=$1
+    local y=$2
+    local count=0
+
+    for ((i=-1; i<=1; i++)); do
+        for ((j=-1; j<=1; j++)); do
+            if [[ $i -eq 0 && $j -eq 0 ]]; then
+                continue
+            fi
+            local ni=$((x+i))
+            local nj=$((y+j))
+
+            if [[ $ni -ge 0 && $ni -lt $ROWS && $nj -ge 0 && $nj -lt $COLS ]]; then
+                count=$((count + grid[$ni,$nj]))
+            fi
+        done
+    done
+
+    echo $count
+}
+
+# Fonction pour appliquer les règles du Jeu de la Vie et générer la nouvelle grille
+NextGen() {
+    for ((i=0; i<$ROWS; i++)); do
+        for ((j=0; j<$COLS; j++)); do
+            local neighbors=$(CountNeighbors $i $j)
+
+            if [[ ${grid[$i,$j]} -eq 1 ]]; then
+                if [[ $neighbors -lt 2 || $neighbors -gt 3 ]]; then
+                    newGrid[$i,$j]=0  # Meurt
+                else
+                    newGrid[$i,$j]=1  # Survit
+                fi
+            else
+                if [[ $neighbors -eq 3 ]]; then
+                    newGrid[$i,$j]=1  # Naît
+                else
+                    newGrid[$i,$j]=0
+                fi
+            fi
+        done
+    done
+
+    # Mise à jour de la grille
+    for ((i=0; i<$ROWS; i++)); do
+        for ((j=0; j<$COLS; j++)); do
+            grid[$i,$j]=${newGrid[$i,$j]}
+        done
+    done
+}
+
+# Exécution du script avec dialogue
+InitGrid
+while true; do
+    DisplayGrid
+    NextGen
+    sleep 0.5  # Pause de 0.5 seconde entre chaque génération pour une meilleure visualisation
+done
