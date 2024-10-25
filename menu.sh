15f224539425ac0cc6cca23a3ea3cba39e60dbf3
ELECOU50 <144897011+ELECOU50@users.noreply.github.com>
2024-10-25 23:40:46 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -8,26 +8,29 @@ menu_principal() {
                 --menu "Choisissez une option:" 19 50 10 \
                 1 "Structures stables" \
                 2 "Oscillateurs" \
-                3 "Vaisseaux" \
-                4 "Mathusalems" \
-                5 "Puffeurs" \
-                6 "Canons" \
-                7 "Jardins d'Eden" \
-                8 "Constructeurs universels" \
-                9 "Quitter" \
+	        3 "Vaisseaux" \
+		4 "Mathusalems" \
+		5 "Puffeurs" \
+		6 "Canons" \
+		7 "Jardins d'Eden" \
+		8 "Constructeur Universel" \
+		9 "Figure personnalisée" \
+		10 "Quitter" \
                 2>&1 >/dev/tty)
 
         case $choix in
-            1) ./Structures_stables.sh ;;
-            2) ./oscillateur.sh ;;
-            3) ./vaisseaux.sh ;;
-            4) ./Mathusalems.sh ;;
-            5) ./Puffeurs.sh ;;
-            6) ./Canon.sh ;;
-            7) ./Jardin_Eden.sh ;;
-            8) ./Constructeurs_Universels.sh ;;
-            9) dialog --clear
-                break ;;
+            1)source ./Structures_stables.sh ;;
+	    2)source ./oscillateur.sh ;;
+	    3)source ./vaisseaux.sh ;;
+            4)source ./Mathusalems.sh ;;
+	    5)source ./Puffeurs.sh ;;
+	    6)source ./Canon.sh ;;
+	    7)source ./Jardin_Eden.sh ;;
+	    8)source ./Constructeurs_Universels.sh ;;
+	    9)source ./Figure.sh ;;
+            10) dialog --clear
+		clear
+		break ;;
             *) dialog --clear --msgbox "Option invalide" 5 40 ;;
         esac
     done
