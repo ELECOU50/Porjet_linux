e29f25f53d89ab6e9e0b147437ed3e8c55cb5079
ELECOU50 <144897011+ELECOU50@users.noreply.github.com>
2024-10-25 23:39:57 +0200

    Update Structures_Stables.sh

--- Structures_Stables.sh
+++ Structures_Stables.sh
@@ -23,8 +23,44 @@ InitGrid(){
     grid[11,26]=1
 }
 
+# Fonction pour afficher la grille dans une boîte de dialogue
+DisplayGrid() {
+    local output=""
+
+    # Dessiner la ligne supérieure de la grille
+    output+="+"
+    for ((j=0; j<$COLS; j++)); do
+        output+="-"
+    done
+    output+="+\n"
+
+    # Dessiner les lignes de la grille avec les cellules
+    for ((i=0; i<$ROWS; i++)); do
+        output+="|"
+        for ((j=0; j<$COLS; j++)); do
+            if [[ ${grid[$i,$j]} -eq 1 ]]; then
+                output+="█"  # Cellule vivante
+            else
+                output+=" "  # Cellule morte (espace vide)
+            fi
+        done
+        output+="|\n"
+    done
+
+    # Dessiner la ligne inférieure de la grille
+    output+="+"
+    for ((j=0; j<$COLS; j++)); do
+        output+="-"
+    done
+    output+="+\n"
+
+    dialog --yesno "$output" 26 56
+    response=$?
+}
+
+
 # Fonction pour compter les voisins vivants d'une cellule
-CountNeighbors() {
+CompteurCellule() {
     local x=$1
     local y=$2
     local count=0
@@ -50,16 +86,16 @@ CountNeighbors() {
 NextGen() {
     for ((i=0; i<$ROWS; i++)); do
         for ((j=0; j<$COLS; j++)); do
-            local neighbors=$(CountNeighbors $i $j)
+            local cellule=$(CompteurCellule $i $j)
 
             if [[ ${grid[$i,$j]} -eq 1 ]]; then
-                if [[ $neighbors -lt 2 || $neighbors -gt 3 ]]; then
+                if [[ $cellule -lt 2 || $cellule -gt 3 ]]; then
                     newGrid[$i,$j]=0  # Meurt
                 else
                     newGrid[$i,$j]=1  # Survit
                 fi
             else
-                if [[ $neighbors -eq 3 ]]; then
+                if [[ $cellule -eq 3 ]]; then
                     newGrid[$i,$j]=1  # Naît
                 else
                     newGrid[$i,$j]=0
@@ -76,28 +112,34 @@ NextGen() {
     done
 }
 
+
 sous_menu1() {
     local sous_menu1
     while true; do
         choix=$(dialog --clear --title "Sous-Menu" \
                 --menu "Choisissez une option:" 15 50 2 \
                 1 "Afficher la grille" \
-                2 "Retourner au menu" \
+		2 "A propos" \
+                3 "Retourner au menu" \
                 2>&1 >/dev/tty)
 
         case $choix in
             1) dialog --clear --msgbox "Affichage de la grille" 5 40
+               # Exécution du script avec dialogue
+               InitGrid
                while true; do
-                        # Exécution du script avec dialogue
-                        InitGrid
-                        while true; do
-                                DisplayGrid
-                                NextGen
-                                sleep 0.5  # Pause de 0.5 seconde entre chaque génération pour une meilleure visualisation
-                        done
-                        break
+               		DisplayGrid
+                        NextGen
+                        sleep 0.1  # Pause de 0.1 seconde entre chaque génération pour une meilleure visualisation
+			if [ $response -ne 0 ]; then
+                        	dialog --clear --msgbox "Retour au menu" 10 40
+                                sous_menu1
+				break
+                        fi
                done ;;
-            2) break ;;
+	    2) dialog --clear --msgbox "Les structures stables sont des ensembles de cellules ayant stoppé toute évolution : elles sont dans un état stationnaire et n’évoluent plus tant qu’aucun élément perturbateur n’apparaît dans leur voisinage. Un bloc de quatre cellules est la plus petite structure stable possible." 20 40
+	       sous_menu1 ;;
+            3) break ;;
             *) dialog --clear --msgbox "Option invalide" 5 40 ;;
         esac
         break
